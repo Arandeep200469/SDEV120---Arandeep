fc475d5ddf8b2e4b703f90e13d2fe0bae3a32bb8
Arandeep200469 <asingh80@ivytech.edu>
2025-04-28 13:47:41 -0400

    Add files via upload

---
+++ raining_cats_and_Dogs.py
@@ -0,0 +1,17 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #This code will:
+>>> # - Create a sentence using these variables
+>>> # - Set variables for "cats" and "dogs"
+>>> # - Print the sentence
+>>> 
+>>> # Set Variables
+>>> c = "cats" # Variables for cats
+>>> d = "dogs" # Variable for dogs
+>>> 
+>>> # Create the sentence
+>>> s = "It is raining " + c + " and " + d + "." # Combine the variables into the sentence
+>>> 
+>>> # Print the sentence
+>>> print(s) # Prints: It is raining cats and dogs.
+It is raining cats and dogs.
